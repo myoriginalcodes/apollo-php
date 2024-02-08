ed8952c9ca040b6bea94bab5935b05b4a71a7908
Edson Nascimento <edna@boozt.com>
2024-02-08 10:11:09 +0100

    Test for required parameters feature

---
+++ tests/Feature/ValidateRequiredTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use MyOriginalCodes\ApolloPhp\Validator;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class ValidateRequiredTest extends TestCase
+{
+    private Validator $validator;
+
+    public function setUp(): void
+    {
+        $this->validator = new Validator();
+    }
+
+    #[Test]
+    public function shouldAskForRequiredParameter(): void
+    {
+        $this->validator->setRules([
+            'field' => ['required']
+        ]);
+
+        $this->validator->validate();
+
+        $this->assertArrayHasKey('field', $this->validator->getErrorsBag());
+    }
+}
